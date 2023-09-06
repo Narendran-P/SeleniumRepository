6e9f5a88602873787d8c6e81b313d252965560b4
Narendran-P <142680634+Narendran-P@users.noreply.github.com>
2023-09-06 22:47:52 +0530

    Delete main/java directory

--- main/java/week3/Assignments/Assignement3/Language.java
+++
@@ -1,7 +0,0 @@
-package week3.Assignments.Assignement3;
-
-public interface Language {
-	
-	public void Java();
-
-}
--- main/java/week3/day1/Ipad.java
+++
@@ -1,8 +0,0 @@
-package week3.day1;
-
-public class Ipad extends IOS {
-
-	public void watchMovie() {
-		System.out.println("Watch Movie");
-	}
-}
